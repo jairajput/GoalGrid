a393a452902d9e8dfdeae4266a67c13181b434ce
Jai <80166407+jairajput@users.noreply.github.com>
2023-10-30 00:33:16 +0530

    Initial Commit

---
+++ GoalGrid/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GoalGrid
+//
+//  Created by Jai  on 30/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GoalGrid/GoalGridApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoalGridApp.swift
+//  GoalGrid
+//
+//  Created by Jai  on 30/10/23.
+//
+
+import SwiftUI
+
+@main
+struct GoalGridApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
